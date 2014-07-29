fc3e3f6fd559db15551647e8d4a9b123cdbe1500
actionless <actionless.loveless@gmail.com>
2014-07-29 13:39:36 +0200

    added readme to run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
-locust -f test.py --host localhost ||
-echo "Did u entered virtualenv and installed all the dependencies from requirements.txt?"
+
+which locust ||
+echo "Did u entered virtualenv and installed all the dependencies from requirements.txt?
+
+1) Create virtuelenv:
+virtualenv -p python2 env
+
+2) Enter virtualenv:
+source ./env/bin/activate
+
+3) Install dependencies:
+pip install -r requirements.txt
+"
+exit 127
+
+locust -f test.py --host localhost "$@"
